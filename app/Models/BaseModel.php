a9c8a5f6dfccf9c11db0ac00ce5df2e5ea36b40c
Yuta Nagamiya <y.nagamiya@gmail.com>
2015-05-11 14:52:14 +0000

    Fix a bug that do not work without the stage

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,12 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model {
+
+	protected function nullIfBlank($value)
+	{
+		return trim($value) !== '' ? $value : null;
+	}
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,11 @@
 <?php namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+class User extends BaseModel implements AuthenticatableContract, CanResetPasswordContract {
 
 	use Authenticatable, CanResetPassword;
 
