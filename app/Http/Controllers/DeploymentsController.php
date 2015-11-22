ad8c1d105ff8fd26428c860ce96b4d7143ab7a10
Yuta Nagamiya <y.nagamiya@gmail.com>
2015-11-22 10:39:37 +0000

    Change to show a completion message after starting the deployment

--- app/Http/Controllers/DeploymentsController.php
+++ app/Http/Controllers/DeploymentsController.php
@@ -9,83 +9,86 @@ use App\Models\Deployment;
 
 use Illuminate\Http\Request;
 
-class DeploymentsController extends Controller {
+class DeploymentsController extends Controller
+{
+    protected $deployment;
 
-	protected $deployment;
+    protected $deploymentForm;
 
-	protected $deploymentForm;
+    /**
+     * Create a new controller instance.
+     *
+     * @param \App\Repositories\Deployment\DeploymentInterface $deployment
+     * @param \App\Services\Form\Deployment\DeploymentForm     $deploymentForm
+     * @return void
+     */
+    public function __construct(DeploymentInterface $deployment, DeploymentForm $deploymentForm)
+    {
+        $this->middleware('auth');
+        $this->middleware('acl');
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @param \App\Repositories\Deployment\DeploymentInterface $deployment
-	 * @param \App\Services\Form\Deployment\DeploymentForm     $deploymentForm
-	 * @return void
-	 */
-	public function __construct(DeploymentInterface $deployment, DeploymentForm $deploymentForm)
-	{
-		$this->middleware('auth');
-		$this->middleware('acl');
+        $this->deployment     = $deployment;
+        $this->deploymentForm = $deploymentForm;
+    }
 
-		$this->deployment     = $deployment;
-		$this->deploymentForm = $deploymentForm;
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Project      $project
+     * @return Response
+     */
+    public function index(Request $request, Project $project)
+    {
+        $page = $request->input('page', 1);
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param \App\Models\Project      $project
-	 * @return Response
-	 */
-	public function index(Request $request, Project $project)
-	{
-		$page = $request->input('page', 1);
+        $perPage = 10;
 
-		$perPage = 10;
+        $deployments = $this->deployment->byProjectId($project->id, $page, $perPage);
 
-		$deployments = $this->deployment->byProjectId($project->id, $page, $perPage);
+        return view('deployments.index')
+            ->with('deployments', $deployments)
+            ->with('project', $project);
+    }
 
-		return view('deployments.index')
-			->with('deployments', $deployments)
-			->with('project', $project);
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Project      $project
+     * @return Response
+     */
+    public function store(Request $request, Project $project)
+    {
+        $input = array_merge($request->all(), [
+            'status'     => null,
+            'message'    => null,
+            'project_id' => $project->id,
+            'user_id'    => $request->user()->id,
+        ]);
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param \App\Models\Project      $project
-	 * @return Response
-	 */
-	public function store(Request $request, Project $project)
-	{
-		$input = array_merge($request->all(), [
-			'status'     => null,
-			'message'    => null,
-			'project_id' => $project->id,
-			'user_id'    => $request->user()->id,
-		]);
+        if ($this->deploymentForm->save($input)) {
+            $deployment = $project->getLastDeployment();
+            $link = link_to_route('projects.deployments.show', "#$deployment->number", [$project, $deployment->number]);
+            $request->session()->flash('status', "The deployment $link was successfully started.");
 
-		if ($this->deploymentForm->save($input)) {
-			return redirect()->route('projects.deployments.index', [$project]);
-		} else {
-			return redirect()->route('projects.deployments.index', [$project])
-				->withInput()
-				->withErrors($this->deploymentForm->errors());
-		}
-	}
+            return redirect()->route('projects.deployments.index', [$project]);
+        } else {
+            return redirect()->route('projects.deployments.index', [$project])
+                ->withInput()
+                ->withErrors($this->deploymentForm->errors());
+        }
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param \App\Models\Project    $project
-	 * @param \App\Models\Deployment $deployment
-	 * @return Response
-	 */
-	public function show(Project $project, Deployment $deployment)
-	{
-		return view('deployments.show')->with('deployment', $deployment);
-	}
-
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Project    $project
+     * @param \App\Models\Deployment $deployment
+     * @return Response
+     */
+    public function show(Project $project, Deployment $deployment)
+    {
+        return view('deployments.show')->with('deployment', $deployment);
+    }
 }
--- app/Services/Form/Deployment/DeploymentForm.php
+++ app/Services/Form/Deployment/DeploymentForm.php
@@ -1,71 +1,74 @@
-<?php namespace App\Services\Form\Deployment;
+<?php
+
+namespace App\Services\Form\Deployment;
 
 use App\Services\Validation\ValidableInterface;
 use App\Services\Deployment\DeployCommanderInterface;
 use App\Repositories\Deployment\DeploymentInterface;
 
-class DeploymentForm {
+class DeploymentForm
+{
+    protected $validator;
 
-	protected $validator;
+    protected $deployment;
 
-	protected $deployment;
+    protected $deployCommander;
 
-	protected $deployCommander;
+    /**
+     * Create a new form service instance.
+     *
+     * @param \App\Services\Validation\ValidableInterface       $validator
+     * @param \App\Repositories\Deployment\DeploymentInterface  $deployment
+     * @param \App\Services\Deployment\DeployCommanderInterface $deployCommander
+     * @return void
+     */
+    public function __construct(ValidableInterface $validator, DeploymentInterface $deployment, DeployCommanderInterface $deployCommander)
+    {
+        $this->validator       = $validator;
+        $this->deployment      = $deployment;
+        $this->deployCommander = $deployCommander;
+    }
 
-	/**
-	 * Create a new form service instance.
-	 *
-	 * @param \App\Services\Validation\ValidableInterface       $validator
-	 * @param \App\Repositories\Deployment\DeploymentInterface  $deployment
-	 * @param \App\Services\Deployment\DeployCommanderInterface $deployCommander
-	 * @return void
-	 */
-	public function __construct(ValidableInterface $validator, DeploymentInterface $deployment, DeployCommanderInterface $deployCommander)
-	{
-		$this->validator       = $validator;
-		$this->deployment      = $deployment;
-		$this->deployCommander = $deployCommander;
-	}
+    /**
+     * Create a new deployment.
+     *
+     * @param array $input Data to create a deployment
+     * @return boolean
+     */
+    public function save(array $input)
+    {
+        if (!$this->valid($input)) {
+            return false;
+        }
 
-	/**
-	 * Create a new deployment.
-	 *
-	 * @param array $input Data to create a deployment
-	 * @return boolean
-	 */
-	public function save(array $input)
-	{
-		if (!$this->valid($input)) {
-			return false;
-		}
+        $deployment = $this->deployment->create($input);
 
-		$deployment = $this->deployment->create($input);
+        if (!$deployment) {
+            return false;
+        }
 
-		if (!$deployment) {
-			return false;
-		}
+        $this->deployCommander->$input['task']($deployment);
 
-		return $this->deployCommander->$input['task']($deployment);
-	}
+        return true;
+    }
 
-	/**
-	 * Return validation errors.
-	 *
-	 * @return array
-	 */
-	public function errors()
-	{
-		return $this->validator->errors();
-	}
+    /**
+     * Return validation errors.
+     *
+     * @return array
+     */
+    public function errors()
+    {
+        return $this->validator->errors();
+    }
 
-	/**
-	 * Test whether form validator passes.
-	 *
-	 * @return boolean
-	 */
-	protected function valid(array $input)
-	{
-		return $this->validator->with($input)->passes();
-	}
-
+    /**
+     * Test whether form validator passes.
+     *
+     * @return boolean
+     */
+    protected function valid(array $input)
+    {
+        return $this->validator->with($input)->passes();
+    }
 }
