2daf1c52bf8c28439255329df14c0418765fa361
Yuta Nagamiya <y.nagamiya@gmail.com>
2015-12-23 14:17:31 +0000

    Add tests

--- tests/Jobs/RollbackTest.php
+++ tests/Jobs/RollbackTest.php
@@ -234,4 +234,248 @@ class RollbackTest extends \TestCase {
 		);
 	}
 
+	public function test_Should_WorkAndSendNotification_When_DeployerIsNormalEndAndEmailNotificationRecipientIsSet()
+	{
+		$deployment = Factory::build('App\Models\Deployment', [
+			'id'         => 1,
+			'project_id' => 1,
+			'number'     => 1,
+			'task'       => 'deploy',
+			'user_id'    => 1,
+			'created_at' => new \Carbon\Carbon,
+			'updated_at' => new \Carbon\Carbon,
+			'user'       => new \App\Models\User,
+		]);
+
+		$updatedDeployment = Factory::build('App\Models\Deployment', [
+			'id'         => 1,
+			'project_id' => 1,
+			'number'     => 1,
+			'task'       => 'deploy',
+			'user_id'    => 1,
+			'created_at' => new \Carbon\Carbon,
+			'updated_at' => new \Carbon\Carbon,
+			'user'       => new \App\Models\User,
+			'status'     => 0,
+		]);
+
+		$recipe = factory::build('app\models\recipe', [
+			'id'          => 1,
+			'name'        => 'recipe 1',
+			'desctiption' => '',
+			'body'        => '',
+		]);
+
+		$project = Factory::build('App\Models\Project', [
+			'id'                           => 1,
+			'name'                         => 'Project 1',
+			'recipe_id'                    => 1,
+			'stage'                        => 'staging',
+			'email_notification_recipient' => 'webloyer@example.com',
+			'created_at'                   => new \Carbon\Carbon,
+			'updated_at'                   => new \Carbon\Carbon,
+			'recipes'                      => [$recipe],
+		]);
+
+		$this->mockProjectRepository
+			->shouldReceive('byId')
+			->once()
+			->andReturn($project);
+
+		$this->mockServerRepository
+			->shouldReceive('byId')
+			->once();
+
+		$this->mockDeploymentRepository
+			->shouldReceive('update')
+			->once();
+
+		$this->mockDeploymentRepository
+			->shouldReceive('byId')
+			->andReturn($updatedDeployment)
+			->once();
+
+		$mockDeployerFile = $this->mock('App\Services\Deployment\DeployerFile')
+			->shouldReceive('getFullPath')
+			->once()
+			->mock();
+
+		$this->mockDeployerFileDirector
+			->shouldReceive('construct')
+			->andReturn($mockDeployerFile)
+			->times(3);
+
+		$this->mockProcess
+			->shouldReceive('run')
+			->once();
+
+		$this->mockProcess
+			->shouldReceive('isSuccessful')
+			->twice()
+			->andReturn(true);
+
+		$this->mockProcess
+			->shouldReceive('getOutput')
+			->once();
+
+		$this->mockProcess
+			->shouldReceive('getExitCode')
+			->once();
+
+		$this->mockProcessBuilder
+			->shouldReceive('add')
+			->times(7)
+			->andReturn($this->mockProcessBuilder);
+
+		$this->mockProcessBuilder
+			->shouldReceive('getProcess')
+			->once()
+			->andReturn($this->mockProcess);
+
+		\Storage::shouldReceive('delete')
+			->times(1)
+			->andReturn(1);
+
+		$this->mockNotifier
+			->shouldReceive('to')
+			->once()
+			->andReturn($this->mockNotifier);
+
+		$this->mockNotifier
+			->shouldReceive('notify')
+			->once();
+
+		$job = new Rollback($deployment);
+
+		$job->handle(
+			$this->mockDeploymentRepository,
+			$this->mockProjectRepository,
+			$this->mockServerRepository,
+			$this->mockProcessBuilder,
+			$this->mockNotifier
+		);
+	}
+
+	public function test_Should_WorkAndSendNotification_When_DeployerIsAbnormalEndAndEmailNotificationRecipientIsSet()
+	{
+		$deployment = Factory::build('App\Models\Deployment', [
+			'id'         => 1,
+			'project_id' => 1,
+			'number'     => 1,
+			'task'       => 'deploy',
+			'user_id'    => 1,
+			'created_at' => new \Carbon\Carbon,
+			'updated_at' => new \Carbon\Carbon,
+			'user'       => new \App\Models\User,
+		]);
+
+		$updatedDeployment = Factory::build('App\Models\Deployment', [
+			'id'         => 1,
+			'project_id' => 1,
+			'number'     => 1,
+			'task'       => 'deploy',
+			'user_id'    => 1,
+			'created_at' => new \Carbon\Carbon,
+			'updated_at' => new \Carbon\Carbon,
+			'user'       => new \App\Models\User,
+			'stauts'     => 1,
+		]);
+
+		$recipe = Factory::build('App\Models\Recipe', [
+			'id'          => 1,
+			'name'        => 'Recipe 1',
+			'desctiption' => '',
+			'body'        => '',
+		]);
+
+		$project = Factory::build('App\Models\Project', [
+			'id'                           => 1,
+			'name'                         => 'Project 1',
+			'recipe_id'                    => 1,
+			'stage'                        => 'staging',
+			'email_notification_recipient' => 'webloyer@example.com',
+			'created_at'                   => new \Carbon\Carbon,
+			'updated_at'                   => new \Carbon\Carbon,
+			'recipes'                      => [$recipe],
+		]);
+
+		$this->mockProjectRepository
+			->shouldReceive('byId')
+			->once()
+			->andReturn($project);
+
+		$this->mockServerRepository
+			->shouldReceive('byId')
+			->once();
+
+		$this->mockDeploymentRepository
+			->shouldReceive('update')
+			->once();
+
+		$this->mockDeploymentRepository
+			->shouldReceive('byId')
+			->andReturn($updatedDeployment)
+			->once();
+
+		$mockDeployerFile = $this->mock('App\Services\Deployment\DeployerFile')
+			->shouldReceive('getFullPath')
+			->once()
+			->mock();
+
+		$this->mockDeployerFileDirector
+			->shouldReceive('construct')
+			->andReturn($mockDeployerFile)
+			->times(3);
+
+		$this->mockProcess
+			->shouldReceive('run')
+			->once();
+
+		$this->mockProcess
+			->shouldReceive('isSuccessful')
+			->twice()
+			->andReturn(false);
+
+		$this->mockProcess
+			->shouldReceive('getErrorOutput')
+			->once();
+
+		$this->mockProcess
+			->shouldReceive('getExitCode')
+			->once();
+
+		$this->mockProcessBuilder
+			->shouldReceive('add')
+			->times(7)
+			->andReturn($this->mockProcessBuilder);
+
+		$this->mockProcessBuilder
+			->shouldReceive('getProcess')
+			->once()
+			->andReturn($this->mockProcess);
+
+		\Storage::shouldReceive('delete')
+			->times(1)
+			->andReturn(1);
+
+		$this->mockNotifier
+			->shouldReceive('to')
+			->once()
+			->andReturn($this->mockNotifier);
+
+		$this->mockNotifier
+			->shouldReceive('notify')
+			->once();
+
+		$job = new Rollback($deployment);
+
+		$job->handle(
+			$this->mockDeploymentRepository,
+			$this->mockProjectRepository,
+			$this->mockServerRepository,
+			$this->mockProcessBuilder,
+			$this->mockNotifier
+		);
+	}
+
 }
