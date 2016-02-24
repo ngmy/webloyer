65340cffc0bdeb2690a9ceed064943be1da735d9
Yuta Nagamiya <y.nagamiya@gmail.com>
2016-02-24 11:21:04 +0000

    Create an installer

---
+++ app/Services/Filesystem/LaravelFilesystem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\Filesystem;
+
+use App\Services\Filesystem\FilesystemInterface;
+
+use Illuminate\Filesystem\Filesystem;
+
+class LaravelFilesystem implements FilesystemInterface
+{
+    protected $fs;
+
+    public function __construct(Filesystem $fs)
+    {
+        $this->fs = $fs;
+    }
+
+    /**
+     * Write a file.
+     *
+     * @param string $path     File path
+     * @param string $contents Contents to write a file
+     * @return mixed
+     */
+    public function put($path, $contents)
+    {
+        return $this->fs->put($path, $contents);
+    }
+
+    /**
+     * Read a file.
+     *
+     * @param string $path File path
+     * @return string Contents
+     */
+    public function get($path)
+    {
+        return $this->fs->get($path);
+    }
+}
