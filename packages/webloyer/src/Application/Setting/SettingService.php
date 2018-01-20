5edc537356936bb95ae0596c42f8adef571467e3
Yuta Nagamiya <y.nagamiya@gmail.com>
2018-01-20 12:13:05 +0900

    add tests

--- packages/webloyer/src/Application/Setting/SettingService.php
+++ packages/webloyer/src/Application/Setting/SettingService.php
@@ -21,6 +21,14 @@ class SettingService
 
     private $mailSettingRepository;
 
+    /**
+     * Create a new application service instance.
+     *
+     * @param \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\AppSettingRepositoryInterface  $appSettingRepository
+     * @param \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\DbSettingRepositoryInterface   $dbSettingRepository
+     * @param \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSettingRepositoryInterface $mailSettingRepository
+     * @return void
+     */
     public function __construct(AppSettingRepositoryInterface $appSettingRepository, DbSettingRepositoryInterface $dbSettingRepository, MailSettingRepositoryInterface $mailSettingRepository)
     {
         $this->appSettingRepository = $appSettingRepository;
@@ -28,20 +36,93 @@ class SettingService
         $this->mailSettingRepository = $mailSettingRepository;
     }
 
+    /**
+     * Get a app setting.
+     *
+     * @return \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\AppSetting
+     */
+    public function getAppSetting()
+    {
+        return $this->appSettingRepository->appSetting();
+    }
+
+    /**
+     * Create or Update app setting.
+     *
+     * @param string $appSettingUrl
+     * @return void
+     */
+    public function saveAppSetting($appSettingUrl)
+    {
+        $appSetting = new AppSetting(
+            $appSettingUrl
+        );
+        $this->appSettingRepository->save($appSetting);
+    }
+
+    /**
+     * Get a db setting.
+     *
+     * @return \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\DbSetting
+     */
+    public function getDbSetting()
+    {
+        return $this->dbSettingRepository->dbSetting();
+    }
+
+    /**
+     * Create or Update a db setting.
+     *
+     * @param string $dbSettingDriver
+     * @param string $dbSettingHost
+     * @param string $dbSettingDatabase
+     * @param string $dbSettingUserName
+     * @param string $dbSettingPassword
+     * @return void
+     */
+    public function saveDbSetting($dbSettingDriver, $dbSettingHost, $dbSettingDatabase, $dbSettingUserName, $dbSettingPassword)
+    {
+        $dbSetting = new DbSetting(
+            new DbSettingDriver($dbSettingDriver),
+            $dbSettingHost,
+            $dbSettingDatabase,
+            $dbSettingUserName,
+            $dbSettingPassword
+        );
+        $this->dbSettingRepository->save($dbSetting);
+    }
+
+    /**
+     * Get a mail setting.
+     *
+     * @return \Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSetting
+     */
     public function getMailSetting()
     {
         return $this->mailSettingRepository->mailSetting();
     }
 
+    /**
+     * Create or Update a mail setting.
+     *
+     * @param string      $mailSettingDriver
+     * @param array       $mailSettingFrom
+     * @param string      $mailSettingSmtpHost
+     * @param int         $mailSettingSmtpPort
+     * @param string|null $mailSettingSmtpEncryption
+     * @param string      $mailSettingSmtpUserName
+     * @param string      $mailSettingSmtpPassword
+     * @param string      $mailSettingSendmailPath
+     * @return void
+     */
     public function saveMailSetting($mailSettingDriver, $mailSettingFrom, $mailSettingSmtpHost, $mailSettingSmtpPort, $mailSettingSmtpEncryption, $mailSettingSmtpUserName, $mailSettingSmtpPassword, $mailSettingSendmailPath)
     {
-        $mailSetting = DB::transaction(function () use ($mailSettingDriver, $mailSettingFrom, $mailSettingSmtpHost, $mailSettingSmtpPort, $mailSettingSmtpEncryption, $mailSettingSmtpUserName, $mailSettingSmtpPassword, $mailSettingSendmailPath) {
+        DB::transaction(function () use ($mailSettingDriver, $mailSettingFrom, $mailSettingSmtpHost, $mailSettingSmtpPort, $mailSettingSmtpEncryption, $mailSettingSmtpUserName, $mailSettingSmtpPassword, $mailSettingSendmailPath) {
             if (is_null($mailSettingSmtpEncryption)) {
                 $mailSettingSmtpEncryption = null;
             } else {
                 $mailSettingSmtpEncryption = new MailSettingSmtpEncryption($mailSettingSmtpEncryption);
             }
-
             $mailSetting = new MailSetting(
                 new MailSettingDriver($mailSettingDriver),
                 $mailSettingFrom,
@@ -52,39 +133,7 @@ class SettingService
                 $mailSettingSendmailPath,
                 $mailSettingSmtpEncryption
             );
-            return $this->mailSettingRepository->save($mailSetting);
+            $this->mailSettingRepository->save($mailSetting);
         });
-
-        return $mailSetting;
-    }
-
-    public function getAppSetting()
-    {
-        return $this->appSettingRepository->appSetting();
-    }
-
-    public function saveAppSetting($appSettingUrl)
-    {
-        $appSetting = new AppSetting(
-            $appSettingUrl
-        );
-        return $this->appSettingRepository->save($appSetting);
-    }
-
-    public function getDbSetting()
-    {
-        return $this->dbSettingRepository->dbSetting();
-    }
-
-    public function saveDbSetting($dbSettingDriver, $dbSettingHost, $dbSettingDatabase, $dbSettingUserName, $dbSettingPassword)
-    {
-        $dbSetting = new DbSetting(
-            new DbSettingDriver($dbSettingDriver),
-            $dbSettingHost,
-            $dbSettingDatabase,
-            $dbSettingUserName,
-            $dbSettingPassword
-        );
-        return $this->dbSettingRepository->save($dbSetting);
     }
 }
---
+++ packages/webloyer/tests/Application/Setting/SettingServiceTest.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Ngmy\Webloyer\Webloyer\Application\Setting;
+
+use Mockery;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\AppSetting;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\AppSettingRepositoryInterface;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\DbSetting;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\DbSettingDriver;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\DbSettingRepositoryInterface;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSetting;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSettingDriver;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSettingRepositoryInterface;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Setting\MailSettingSmtpEncryption;
+use TestCase;
+use Tests\Helpers\MockeryHelper;
+
+class SettingServiceTest extends TestCase
+{
+    use MockeryHelper;
+
+    private $settingService;
+
+    private $appSettingRepository;
+
+    private $dbSettingRepository;
+
+    private $mailSettingRepository;
+
+    private $inputForSaveAppSetting = [
+        'appSettingUrl' => '',
+    ];
+
+    private $inputForSaveDbSetting = [
+        'dbSettingDriver'   => 'mysql',
+        'dbSettingHost'     => '',
+        'dbSettingDatabase' => '',
+        'dbSettingUserName' => '',
+        'dbSettingPassword' => '',
+    ];
+
+    private $inputForSaveMailSetting = [
+        'mailSettingDriver'         => 'smtp',
+        'mailSettingFrom'           => [],
+        'mailSettingSmtpHost'       => '',
+        'mailSettingSmtpPort'       => '',
+        'mailSettingSmtpEncryption' => null,
+        'mailSettingSmtpUserName'   => '',
+        'mailSettingSmtpPassword'   => '',
+        'mailSettingSendmailPath'   => '',
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->appSettingRepository = $this->mock(AppSettingRepositoryInterface::class);
+        $this->dbSettingRepository = $this->mock(DbSettingRepositoryInterface::class);
+        $this->mailSettingRepository = $this->mock(MailSettingRepositoryInterface::class);
+        $this->settingService = new SettingService(
+            $this->appSettingRepository,
+            $this->dbSettingRepository,
+            $this->mailSettingRepository
+        );
+    }
+
+    public function tests_Should_GetAppSetting()
+    {
+        $expectedResult = true;
+        $this->appSettingRepository
+            ->shouldReceive('appSetting')
+            ->withNoArgs()
+            ->andReturn($expectedResult)
+            ->once();
+
+        $actualResult = $this->settingService->getAppSetting();
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function tests_Should_SaveAppSetting()
+    {
+        $this->appSettingRepository
+            ->shouldReceive('save')
+            ->with(Mockery::on(function ($arg) {
+                extract($this->inputForSaveAppSetting);
+                $appSetting = new AppSetting(
+                    $appSettingUrl
+                );
+                return $arg == $appSetting;
+            }))
+            ->once();
+
+        extract($this->inputForSaveAppSetting);
+
+        $this->settingService->saveAppSetting(
+            $appSettingUrl
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function tests_Should_GetDbSetting()
+    {
+        $expectedResult = true;
+        $this->dbSettingRepository
+            ->shouldReceive('dbSetting')
+            ->withNoArgs()
+            ->andReturn($expectedResult)
+            ->once();
+
+        $actualResult = $this->settingService->getDbSetting();
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function tests_Should_SaveDbSetting()
+    {
+        $this->dbSettingRepository
+            ->shouldReceive('save')
+            ->with(Mockery::on(function ($arg) {
+                extract($this->inputForSaveDbSetting);
+                $dbSetting = new DbSetting(
+                    new DbSettingDriver($dbSettingDriver),
+                    $dbSettingHost,
+                    $dbSettingDatabase,
+                    $dbSettingUserName,
+                    $dbSettingPassword
+                );
+                return $arg == $dbSetting;
+            }))
+            ->once();
+
+        extract($this->inputForSaveDbSetting);
+
+        $this->settingService->saveDbSetting(
+            $dbSettingDriver,
+            $dbSettingHost,
+            $dbSettingDatabase,
+            $dbSettingUserName,
+            $dbSettingPassword
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function tests_Should_GetMailSetting()
+    {
+        $expectedResult = true;
+        $this->mailSettingRepository
+            ->shouldReceive('mailSetting')
+            ->withNoArgs()
+            ->andReturn($expectedResult)
+            ->once();
+
+        $actualResult = $this->settingService->getMailSetting();
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function tests_Should_SaveMailSetting_When_MailSettingEncryptionIsNull()
+    {
+        $this->checkSaveMailSetting();
+    }
+
+    public function tests_Should_SaveMailSetting_When_MailSettingEncryptionIsNotNull()
+    {
+        $this->inputForSaveMailSetting['mailSettingSmtpEncryption'] = 'tls';
+
+        $this->checkSaveMailSetting();
+    }
+
+    private function checkSaveMailSetting()
+    {
+        $this->mailSettingRepository
+            ->shouldReceive('save')
+            ->with(Mockery::on(function ($arg) {
+                extract($this->inputForSaveMailSetting);
+                if (is_null($mailSettingSmtpEncryption)) {
+                    $mailSettingSmtpEncryption = null;
+                } else {
+                    $mailSettingSmtpEncryption = new MailSettingSmtpEncryption($mailSettingSmtpEncryption);
+                }
+                $mailSetting = new MailSetting(
+                    new MailSettingDriver($mailSettingDriver),
+                    $mailSettingFrom,
+                    $mailSettingSmtpHost,
+                    $mailSettingSmtpPort,
+                    $mailSettingSmtpUserName,
+                    $mailSettingSmtpPassword,
+                    $mailSettingSendmailPath,
+                    $mailSettingSmtpEncryption
+                );
+                return $arg == $mailSetting;
+            }))
+            ->once();
+
+        extract($this->inputForSaveMailSetting);
+
+        $this->settingService->saveMailSetting(
+            $mailSettingDriver,
+            $mailSettingFrom,
+            $mailSettingSmtpHost,
+            $mailSettingSmtpPort,
+            $mailSettingSmtpEncryption,
+            $mailSettingSmtpUserName,
+            $mailSettingSmtpPassword,
+            $mailSettingSendmailPath
+        );
+
+        $this->assertTrue(true);
+    }
+}
