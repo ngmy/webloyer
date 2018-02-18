3e9cf5ed1bf3c0ec0cbdd4fd2815d44364bd4088
Yuta Nagamiya <y.nagamiya@gmail.com>
2018-02-18 23:30:29 +0900

    add tests

---
+++ packages/webloyer/tests/Domain/Model/Deployer/DeployerFileDirectorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ngmy\Webloyer\Webloyer\Domain\Model\Deployer;
+
+use Ngmy\Webloyer\Webloyer\Domain\Model\Deployer\DeployerFile;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Deployer\DeployerFileBuilderInterface;
+use Ngmy\Webloyer\Webloyer\Domain\Model\Deployer\DeployerFileDirector;
+use TestCase;
+use Tests\Helpers\MockeryHelper;
+
+class DeployerFileDirectorTest extends TestCase
+{
+    use MockeryHelper;
+
+    private $deployerFileDirector;
+
+    private $fileBuilder;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fileBuilder = $this->mock(DeployerFileBuilderInterface::class);
+        $this->deployerFileDirector = new DeployerFileDirector($this->fileBuilder);
+    }
+
+    public function test_Should_ConstructDeployerFileInstance()
+    {
+        $expectedResult = $this->mock(DeployerFile::class);
+
+        $this->fileBuilder
+            ->shouldReceive('pathInfo')
+            ->withNoArgs()
+            ->once();
+        $this->fileBuilder
+            ->shouldReceive('put')
+            ->withNoArgs()
+            ->once();
+        $this->fileBuilder
+            ->shouldReceive('getResult')
+            ->withNoArgs()
+            ->andReturn($expectedResult)
+            ->once();
+
+        $actualResult = $this->deployerFileDirector->construct();
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+}
