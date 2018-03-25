bc32d60dad3d0e2487422f781bb8f7e44d0a0cea
Yuta Nagamiya <y.nagamiya@gmail.com>
2018-03-25 15:35:03 +0900

    add test

---
+++ packages/webloyer/tests/Domain/Model/ConcurrencySafeTraitTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Ngmy\Webloyer\Webloyer\Domain\Model;
+
+use RuntimeException;
+use Ngmy\Webloyer\Webloyer\Domain\Model\ConcurrencySafeTrait;
+use TestCase;
+
+class ConcurrencySafeTraitTest extends TestCase
+{
+    public function test_Should_GetConcurrencyVersion()
+    {
+        $expectedResult = 'some concurrency version';
+
+        $anonymousUsingConcurrencySafeTrait = $this->createAnonymousUsingConcurrencySafeTrait([
+            'concurrencyVersion' => $expectedResult,
+        ]);
+
+        $actualResult = $anonymousUsingConcurrencySafeTrait->concurrencyVersion();
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function test_Should_FailWhenConcurrencyViolationThrowException_When_ConcurrencyViolation()
+    {
+        $concurrencyVersion1 = 'concurrency version 1';
+        $concurrencyVersion2 = 'concurrency version 2';
+
+        $anonymousUsingConcurrencySafeTrait = $this->createAnonymousUsingConcurrencySafeTrait([
+            'concurrencyVersion' => $concurrencyVersion1,
+        ]);
+
+        $anonymousUsingConcurrencySafeTrait->failWhenConcurrencyViolation($concurrencyVersion2);
+    }
+
+    public function test_Should_FailWhenConcurrencyViolationNotThrowException_When_ConcurrencyViolation()
+    {
+        $concurrencyVersion = 'some concurrency version';
+
+        $anonymousUsingConcurrencySafeTrait = $this->createAnonymousUsingConcurrencySafeTrait([
+            'concurrencyVersion' => $concurrencyVersion,
+        ]);
+
+        try {
+            $actualResult = $anonymousUsingConcurrencySafeTrait->failWhenConcurrencyViolation($concurrencyVersion);
+        } catch (RuntimeException $e) {
+            $this->fail($e->getMessage());
+        }
+
+        $this->assertNull($actualResult);
+    }
+
+    public function createAnonymousUsingConcurrencySafeTrait(array $params)
+    {
+        $concurrencyVersion = '';
+
+        extract($params);
+
+        return new class($concurrencyVersion) {
+            use ConcurrencySafeTrait;
+
+            public function __construct($concurrencyVersion)
+            {
+                $this->setConcurrencyVersion($concurrencyVersion);
+            }
+        };
+    }
+}
