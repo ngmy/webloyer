ce94b9c279fd196ae3133cbf7ee6a0de1cb67954
Yuta Nagamiya <y.nagamiya@gmail.com>
2018-04-04 18:44:00 +0900

    add tests

---
+++ packages/identity_access/tests/Domain/Model/User/HasPermissionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ngmy\Webloyer\IdentityAccess\Domain\Model\User;
+
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\HasPermission;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\User;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\UserRepositoryInterface;
+use Tests\Helpers\MockeryHelper;
+use TestCase;
+
+class HasPermissionTest extends TestCase
+{
+    use MockeryHelper;
+
+    /**
+     * @dataProvider isProvider
+     */
+    public function test_Should_VerifyUserIsSpecifiedPermission_When_($permission, $operator)
+    {
+        $user = $this->createUserUsingHasPermission();
+        $expectedResult = true;
+
+        $userRepository = $this->mock(UserRepositoryInterface::class);
+        $userRepository
+            ->shouldReceive('can')
+            ->with($user, $permission, $operator)
+            ->andReturn($expectedResult)
+            ->once();
+        $this->app->instance(UserRepositoryInterface::class, $userRepository);
+
+        $actualResult = $user->can($permission, $operator);
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function isProvider()
+    {
+        return [
+            ['some permission', null],
+            ['some permission', 'OR'],
+        ];
+    }
+
+    public function createUserUsingHasPermission()
+    {
+        return new class() extends User {
+            use HasPermission;
+
+            public function __construct()
+            {
+            }
+        };
+    }
+}
