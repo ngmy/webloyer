fa6504c3c361fdef5b15d5e261b2c048ee0497a2
Yuta Nagamiya <y.nagamiya@gmail.com>
2018-04-04 18:37:28 +0900

    add tests

--- packages/identity_access/src/Domain/Model/User/HasRole.php
+++ packages/identity_access/src/Domain/Model/User/HasRole.php
@@ -3,11 +3,12 @@
 namespace Ngmy\Webloyer\IdentityAccess\Domain\Model\User;
 
 use App;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\Role\RoleSlug;
 use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\UserRepositoryInterface;
 
 trait HasRole
 {
-    public function is($slug, $operator = null)
+    public function is(RoleSlug $slug, $operator = null)
     {
         $userRepository = App::make(UserRepositoryInterface::class);
 
---
+++ packages/identity_access/tests/Domain/Model/User/HasRoleTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Ngmy\Webloyer\IdentityAccess\Domain\Model\User;
+
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\Role\RoleSlug;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\HasRole;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\User;
+use Ngmy\Webloyer\IdentityAccess\Domain\Model\User\UserRepositoryInterface;
+use Tests\Helpers\MockeryHelper;
+use TestCase;
+
+class HasRoleTest extends TestCase
+{
+    use MockeryHelper;
+
+    /**
+     * @dataProvider isProvider
+     */
+    public function test_Should_VerifyUserIsSpecifiedRole_When_($slug, $operator)
+    {
+        $user = $this->createUserUsingHasRole();
+        $expectedResult = true;
+
+        $userRepository = $this->mock(UserRepositoryInterface::class);
+        $userRepository
+            ->shouldReceive('is')
+            ->with($user, $slug, $operator)
+            ->andReturn($expectedResult)
+            ->once();
+        $this->app->instance(UserRepositoryInterface::class, $userRepository);
+
+        $actualResult = $user->is($slug, $operator);
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+
+    public function isProvider()
+    {
+        return [
+            [new RoleSlug('administrator'), null],
+            [new RoleSlug('administrator'), 'OR'],
+        ];
+    }
+
+    public function createUserUsingHasRole()
+    {
+        return new class() extends User {
+            use HasRole;
+
+            public function __construct()
+            {
+            }
+        };
+    }
+}
