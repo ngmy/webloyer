c60e39649deab0c1de2a6cc9dfb4b31e9e189efb
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-04-05 22:05:58 +0900

    fix breadcrumbs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -75,7 +75,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                {!! Breadcrumbs::renderIfExists() !!}
+                {!! Breadcrumbs::exists() ? Breadcrumbs::render() : '' !!}
             </div>
         </div>
     </div>
