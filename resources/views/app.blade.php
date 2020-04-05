ee6534e8c576989ff1a42ed2198d0b221b74ad50
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-04-05 22:16:06 +0900

    fix auth url

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -54,7 +54,7 @@
 
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::guest())
-                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/login') }}">Login</a></li>
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
