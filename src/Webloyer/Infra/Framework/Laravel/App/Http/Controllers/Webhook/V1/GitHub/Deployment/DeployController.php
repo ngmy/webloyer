23d1a86791e80f4b42e9a7f5c8800940c3a02b02
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-06-08 23:42:41 +0900

    rename

---
+++ src/Webloyer/Infra/Framework/Laravel/App/Http/Controllers/Webhook/V1/GitHub/Deployment/DeployController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webloyer\Infra\Framework\Laravel\App\Http\Controllers\Webhook\V1\GitHub\Deployment;
+
+use Illuminate\Http\Request;
+use Webloyer\App\Service\Deployment\CreateDeploymentRequest;
+
+class DeployController extends BaseController
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @param string  $projectId
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, string $projectId)
+    {
+        $serviceRequest = (new CreateDeploymentRequest())
+            ->setProjectId($projectId)
+            ->setExecutor($request->user()->toEntity()->id());
+        $deployment = $this->service->execute($serviceRequest);
+
+        $link = link_to_route('projects.deployments.show', '#' . $deployment->number, [$projectId, $deployment->number]);
+        $request->session()->flash('status', "The deployment $link was successfully started.");
+
+        return redirect()->route('projects.deployments.index', [$projectId]);
+    }
+}
--- src/Webloyer/Infra/Framework/Laravel/routes/webhook.php
+++ src/Webloyer/Infra/Framework/Laravel/routes/webhook.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::middleware('github_webhook_secret')->namespace('Github')->prefix('github')->group(function () {
-    Route::namespace('V1')->prefix('v1')->group(function () {
+Route::namespace('V1')->prefix('v1')->group(function () {
+    Route::middleware('github_webhook_secret')->namespace('GitHub')->prefix('github')->group(function () {
         Route::resource('projects.deployments', 'DeploymentController')->only([
             'store',
         ]);
