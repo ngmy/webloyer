0d0a21e9bdd223b5da7dde1bd92ee5dae036cefd
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-06-16 21:57:47 +0900

    findbugs

--- src/Webloyer/Infra/Framework/Laravel/routes/webhook.php
+++ src/Webloyer/Infra/Framework/Laravel/routes/webhook.php
@@ -3,8 +3,8 @@
 Route::prefix('v1')->namespace('V1')->group(function () {
     Route::middleware('github_webhook_secret')->namespace('GitHub')->prefix('github')->group(function () {
         Route::namespace('Deployment')->group(function () {
-            Route::post('projects/{project}/deployments/deploy', 'DeployController')->middleware('response_json')->name('webhook.v1.github.projects.deployments.deploy');
-            Route::post('projects/{project}/deployments/rollback', 'RollbackController')->middleware('response_json')->name('webhook.v1.github.projects.deployments.rollback');
+            Route::post('projects/{project}/deployments/deploy', 'DeployController')->name('webhook.v1.github.projects.deployments.deploy')->middleware('response_json');
+            Route::post('projects/{project}/deployments/rollback', 'RollbackController')->name('webhook.v1.github.projects.deployments.rollback')->middleware('response_json');
         });
     });
 });
