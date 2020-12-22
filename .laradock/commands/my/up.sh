c98f068a66dc5aaac38a9daecddbcaa71930c23a
Yuta Nagamiya <y.nagamiya@gmail.com>
2020-12-22 17:43:54 +0900

    fix command

--- .laradock/commands/my/up.sh
+++ .laradock/commands/my/up.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -Ceuo pipefail
 
-NAME='my:up'
-DESCRIPTION='Launch my development environment'
+local NAME='my:up'
+local DESCRIPTION='Launch my development environment'
 
 handle() {
   cp -f ../.laradock/env-development .env
   docker-compose up -d nginx mysql mailhog workspace
   cp ../.env.development ../.env
-  laradockctl workspace:composer install
-  laradockctl laravel:artisan key:generate
-  laradockctl laravel:artisan migrate
-  laradockctl workspace:npm install
-  laradockctl workspace:npm install
-  laradockctl workspace:npm run dev
+  docker-compose exec workspace composer install
+  docker-compose exec workspace php artisan key:generate
+  docker-compose exec workspace php artisan migrate
+  docker-compose exec workspace npm install
+  docker-compose exec workspace npm run dev
 }
